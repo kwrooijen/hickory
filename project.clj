47d912f7b54e743c10721016c9570cd4724ac6a6
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2021-01-24 13:03:14 +0100

    Remove clojurescript

--- project.clj
+++ project.clj
@@ -6,46 +6,16 @@
 
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
                  [org.jsoup/jsoup "1.9.2"]
                  [viebel/codox-klipse-theme "0.0.1"]
                  [quoin "0.1.2" :exclusions [org.clojure/clojure]]]
 
-  :hooks [leiningen.cljsbuild]
-
   :plugins [[lein-codox "0.10.0"]]
   :doo {:build "test"}
 
-  :source-paths ["src/clj" "src/cljc" "src/cljs"]
-
-  :cljsbuild {:builds        {}
-              :test-commands {"unit-tests" ["lein" "with-profile" "test" "doo" "phantom" "once"]}}
+  :source-paths ["src/clj" "src/cljc"]
 
   :profiles
   {:dev  {:source-paths ["src/clj" "src/cljc"]
           :dependencies [[lein-doo "0.1.7"]]
-          :plugins      [[lein-cljsbuild "1.1.4"]
-                         [lein-doo "0.1.7" :exclusions [org.clojure/clojure]]]}
-   :test {:source-paths ["src/cljs" "src/cljc" "test/cljc" "test/cljs"]
-          :plugins      [[lein-doo "0.1.7" :exclusions [org.clojure/clojure]]]
-          :cljsbuild    {:builds {:test {:source-paths  ["src/cljs" "src/cljc" "test/cljc" "test/cljs"]
-                                         :compiler      {:output-to "target/cljs/testable.js"
-                                                         :main      "hickory.doo-runner"}
-                                         :optimizations :whitespace
-                                         :pretty-print  true}}}
-          }}
-
-  :codox {:sources                   ["src" "target/generated-src"]
-          :output-path                "codox-out"
-          :src-dir-uri               "http://github.com/davidsantiago/hickory/blob/master"
-          :metadata {:doc/format :markdown}
-          :language :clojurescript
-          :themes [:default [:klipse {:klipse/external-libs  "https://raw.githubusercontent.com/davidsantiago/hickory/master/src/cljc,https://raw.githubusercontent.com/davidsantiago/hickory/master/src/cljs"
-                                      :klipse/require-statement "(ns my.html
-                                                                  (:require [hickory.core :refer [parse as-hiccup as-hickory]]
-                                                                            [hickory.render :refer [hickory-to-html hiccup-to-html]]
-                                                                            [hickory.convert :refer [hiccup-to-hickory]]))
-                                                                "}]]
-          :src-linenum-anchor-prefix "L"}
-
-  )
+          :plugins      [[lein-doo "0.1.7" :exclusions [org.clojure/clojure]]]}})
